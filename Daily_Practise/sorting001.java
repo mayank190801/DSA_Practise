4b8f00c8e559f3d932d3208c78f9ae9592a0bfde
Mayank Singh <mayank190801@gmail.com>
2023-02-11 11:01:51 +0530

    Selection Sort

--- Daily_Practise/sorting001.java
+++ Daily_Practise/sorting001.java
@@ -5,7 +5,8 @@ public class sorting001 {
         System.out.println("hey there I am coding in java back again!!");
         //time space stability place application  --- these are the stuff you should know
         int[] arr = { 2, 7, 10, 1, 4,  8, 5, 3};
-        insertionSort(arr);
+//        insertionSort(arr);
+        selectionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -21,4 +22,23 @@ public class sorting001 {
             arr[k] = curr;
         }
     }
+
+    public static void selectionSort(int[] arr){
+        //find the smallest element, and then replace it
+        for(int i = 0; i < arr.length - 1; i++){
+            int ri = i;
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[j] < arr[ri]) ri = j;
+            }
+            swap(i, ri, arr);
+        }
+    }
+
+    public static void swap(int i, int j, int[] arr){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
 }
