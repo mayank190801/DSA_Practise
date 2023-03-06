9b50ad7dc46b93b5bcf51141a829d1aaa85ced5a
Mayank Singh <mayank190801@gmail.com>
2023-03-06 08:24:38 +0530

    leetcode1476

---
+++ Daily_Practise/leetcode1476.java
@@ -0,0 +1,28 @@
+public class leetcode1476 {
+    public static void main(String[] args) {
+
+    }
+
+    class SubrectangleQueries {
+
+        int[][] rectangle;
+        public SubrectangleQueries(int[][] rectangle) {
+            this.rectangle = rectangle;
+        }
+
+        public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+            for(int i = row1; i <= row2; i++){
+                for(int j = col1; j <= col2; j++){
+                    rectangle[i][j] = newValue;
+                }
+            }
+
+        }
+
+        public int getValue(int row, int col) {
+            return rectangle[row][col];
+        }
+    }
+
+
+}
