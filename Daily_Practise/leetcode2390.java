4679ec450200d2c361d49086ee5441413d4f865d
Mayank Singh <mayank190801@gmail.com>
2023-04-11 09:51:11 +0530

    solved removing stars from a string

--- Daily_Practise/leetcode2390.java
+++ Daily_Practise/leetcode2390.java
@@ -1,2 +1,24 @@
-public class leetcode2390 {
+import java.util.Stack;
+
+class leetcode2390 {
+
+    public static void main(String[] args) {
+        String ans = removeStars("leet**cod*e");
+        System.out.println(ans);
+    }
+
+    public static String removeStars(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '*'){
+                stack.pop();
+            }else stack.push(ch);
+        }
+        StringBuilder ans = new StringBuilder();
+        while(!stack.isEmpty()) {
+            ans.append(stack.pop());
+        }
+        return ans.reverse().toString();
+    }
+
 }
