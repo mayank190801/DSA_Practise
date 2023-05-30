212c5b80b00c598334a817687a6110e9f60f0f7a
Mayank Singh <mayank190801@gmail.com>
2023-05-30 21:44:52 +0530

    update in code

--- Problem_Solving/may28que1.java
+++ Problem_Solving/may28que1.java
@@ -1,6 +1,7 @@
 public class may28que1 {
     public static void main(String[] args) {
         System.out.println("Let's do it brother! change your life");
+        //for sure my man! -- let's figure this out, how can we do it?? think about it 
     }
     
 }
