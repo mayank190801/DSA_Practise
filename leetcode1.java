39f1e6de3afaf2ba09654bd346ee19416319bf93
Mayank Singh <mayank190801@gmail.com>
2023-10-25 21:43:46 +0530

    solved Minimum Sum of Mountain Triplets I - leetcode

---
+++ leetcode1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumSum(int[] nums) {
+        int len = nums.length;
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < len - 2; i++){
+            for(int j = i + 1; j < len - 1; j++){
+                for(int k = j + 1; k < len; k++){
+                    //so in this case i, j, k will always be different for sur e
+                    // sum should be the least
+                    if(nums[i] < nums[j]  && nums[j] > nums[k]){
+                        ans = Math.min(ans, nums[i] + nums[j] + nums[k]);
+                    }
+                }
+            }
+        }
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+}
