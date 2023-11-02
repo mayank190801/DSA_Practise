a42f9478686151adcb7bd6cb0448c3960d143e14
Mayank Singh <mayank190801@gmail.com>
2023-11-02 23:50:15 +0530

    solved more cleaner version Visit Array Positions to Maximize Score leetcode

--- leetcode21.java
+++ leetcode21.java
@@ -5,21 +5,17 @@ public class leetcode21 {
     }
 
     //so this should makes more sense
+    //I did solve it, but it looks ugly as fuck, I want to make it 10x better, how can I do that?
+
     public long maxScore(int[] nums, int x) {
-        long evenMax = 0;
-        long oddMax = 0;
-        int len = nums.length;
-        if(nums[0]%2 == 0){
-            evenMax = nums[0];
-            oddMax = Integer.MIN_VALUE + 10_000_000;
-        }else{
-            oddMax = nums[0];
-            evenMax = Integer.MIN_VALUE + 10_000_000;
-        }
-        for(int i = 1; i < len; i++){
-            boolean even = nums[i] % 2 == 0;
-            long newMax = nums[i] + Math.max(evenMax - (!even ? x : 0) , oddMax - (even ? x : 0));
-            if(even) evenMax = newMax; else oddMax = newMax;
+        long evenMax = (nums[0] & 1) == 0 ? nums[0] : Integer.MIN_VALUE + 10_000_000;
+        long oddMax = (nums[0] & 1) != 0 ? nums[0] : Integer.MIN_VALUE + 10_000_000;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] % 2== 0){
+                evenMax = nums[i] + Math.max(evenMax, oddMax - x);
+            }else{
+                oddMax = nums[i] + Math.max(oddMax, evenMax - x);
+            }
         }
         return Math.max(evenMax, oddMax);
     }
