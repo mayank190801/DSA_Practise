464c5a827c44790d437d5041167ee62e43266857
Mayank Singh <mayank190801@gmail.com>
2023-11-06 22:00:29 +0530

    solved Maximum Number of Jumps to Reach the Last Index leetcode

--- leetcode25.java
+++ leetcode25.java
@@ -1,21 +1,22 @@
+import java.util.Arrays;
+
 public class leetcode25 {
     //this one is going to be O(n^2)
 
     public static void main(String[] args) {
-
+        //this could work or not, idk
     }
 
     public static int maximumJumps(int[] nums, int target) {
         int len = nums.length;
         int[] jumps = new int[len];
-        //final ans will be written for index 0 only
+        Arrays.fill(jumps, -1);
+        jumps[len - 1] = 0;
         for(int i = len - 2; i >= 0; i--){
-            int curr = -1;
            for(int j = i + 1; j < len; j++){
                if(jumps[j] == -1) continue;
-               if(Math.abs(nums[i] - nums[j]) <= target) curr = Math.max(curr, jumps[j] + 1);
+               if(Math.abs(nums[i] - nums[j]) <= target) jumps[i] = Math.max(jumps[i], jumps[j] + 1);
            }
-           jumps[i] = curr;
         }
         return jumps[0];
     }
